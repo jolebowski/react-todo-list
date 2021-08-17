04f965f5b85a1e0ffc036698bbbec5b1a0413f63
jolebowski <jordan.moreira@epitech.eu>
2021-08-17 03:16:19 +0200

    func add task and add new package uuid

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import Item from './Item';
 export default function Form() {
   const [dataArr, setDataArr] = useState([
@@ -11,19 +13,37 @@ export default function Form() {
     { id: 4, text: 'kevin' },
     { id: 5, text: 'kevin' },
   ]);
+  const [text, setText] = useState('');
 
   const deleteTask = (id) => {
     const taskDelete = dataArr.filter((d) => d.id !== id);
     setDataArr(taskDelete);
   };
 
+  const addTask = (e) => {
+    e.preventDefault();
+    const newArr = [...dataArr];
+    const newTodo = {};
+
+    newTodo.text = text;
+    newTodo.id = uuidv4();
+    
+    newArr.push(newTodo);
+    setDataArr(newArr);
+    
+    setText('');
+  };
+
   return (
     <div className='mb-4  md:flex-col justify-center items-center'>
-      <form className='mb-3'>
+      <form className='mb-3' onSubmit={(e) => addTask(e)}>
         <label htmlFor='todo' className='block text-gray-700 mb-3'>
           Chose à faire !
         </label>
         <input
+          required
+          onChange={(e) => setText(e.target.value)}
+          value={text}
           type='text'
           id='todo'
           name='todo'
